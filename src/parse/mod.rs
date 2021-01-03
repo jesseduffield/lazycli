b837f391facbcb6054d3d62db8de3b6c9c2d43ab
Jesse Duffield <jessedduffield@gmail.com>
2021-01-03 20:55:26 +1100

    better parsing

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,7 +1,45 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
-fn parse(text: String) -> Vec<usize> {
+#[derive(PartialEq, Debug)]
+struct Row {
+  original_line: String,
+  cells: Vec<String>,
+}
+
+impl Row {
+  pub fn new(original_line: String, cells: Vec<String>) -> Row {
+    Row {
+      original_line,
+      cells,
+    }
+  }
+}
+
+fn parse(text: String) -> Vec<Row> {
+  let column_indices = get_column_indices(&text);
+
+  text
+    .lines()
+    .map(|line| {
+      let mut cells = vec![];
+
+      let mut remainder = line;
+      let mut last = 0;
+      for position in &column_indices[1..] {
+        let (cell, rest) = remainder.split_at(position - last);
+        remainder = rest;
+        cells.push(cell.trim_end().to_owned());
+        last = *position;
+      }
+      cells.push(remainder.trim_end().to_owned());
+
+      Row::new(line.to_owned(), cells)
+    })
+    .collect()
+}
+
+fn get_column_indices(text: &String) -> Vec<usize> {
   // split into lines, see where the columns begin.
 
   // we need a map of all the places where there are spaces. for each line we will remove items from this map.
@@ -57,41 +95,169 @@ fn parse(text: String) -> Vec<usize> {
 #[cfg(test)]
 mod tests {
   use super::*;
+  use pretty_assertions::assert_eq;
 
   #[test]
-  fn test_docker_ps() {
+  fn test_parse_docker_ps() {
     let text = "CONTAINER ID   IMAGE                       COMMAND              CREATED       STATUS          PORTS                              NAMES\n\
                 17c523089229   aa                          \"./ops/dev/api…\"   2 weeks ago   Up 43 seconds   0.0.0.0:20->80/tcp                 blah\n\
                 dcddf219bb2b   bb                          \"./ops/dev/sid…\"   2 weeks ago   Up 44 seconds                                      blah-sidekiq_2\n\
                 43484e7c2774   dd:latest                   \"ops/dev/proxy…\"   2 weeks ago   Up 46 seconds   0.0.0.0:80->80/tcp                 blah-proxy_4\n\
-                f13385e55986   cc:1.4.6-alpine             \"/docker.…\"        2 weeks ago   Up 46 seconds   0.0.0.0:9300->9300/tcp, 9300/tcp   blah.2_1\n\
-                bc75169bb13e   mysql:1.6                   \"docker.s…\"        2 weeks ago   Up 46 seconds   0.0.0.0:12306->3406/tcp            blah.6_1\n\
-                1cec55fcafbc   a-aaa-a:latest              \"docker.s…\"        2 weeks ago   Up 46 seconds   0.0.0.0:41005->9112/tcp            blah\n\
                 8a61b6cc2d3b   aaaaa:4.0.3-alpine          \"docker.s…\"        2 weeks ago   Up 46 seconds   0.0.0.0:6300->6322/tcp             blah.99_1\n";
+
+    assert_eq!(parse(String::from(text)), vec![
+          Row {
+                original_line: String::from("CONTAINER ID   IMAGE                       COMMAND              CREATED       STATUS          PORTS                              NAMES"),
+                cells: vec![
+                    String::from("CONTAINER ID"),
+                    String::from("IMAGE"),
+                    String::from("COMMAND"),
+                    String::from(""),
+                    String::from("CREATED"),
+                    String::from(""),
+                    String::from("STATUS"),
+                    String::from("PORTS"),
+                    String::from("NAMES"),
+                ],
+            },
+            Row {
+                original_line: String::from("17c523089229   aa                          \"./ops/dev/api…\"   2 weeks ago   Up 43 seconds   0.0.0.0:20->80/tcp                 blah"),
+                cells: vec![
+                    String::from("17c523089229"),
+                    String::from("aa"),
+                    String::from("\"./ops/dev/api…"),
+                    String::from("\""),
+                    String::from("2 weeks"),
+                    String::from("ago"),
+                    String::from("Up 43 seconds"),
+                    String::from("0.0.0.0:20->80/tcp"),
+                    String::from("blah"),
+                ],
+            },
+            Row {
+                original_line: String::from("dcddf219bb2b   bb                          \"./ops/dev/sid…\"   2 weeks ago   Up 44 seconds                                      blah-sidekiq_2"),
+                cells: vec![
+                    String::from("dcddf219bb2b"),
+                    String::from("bb"),
+                    String::from("\"./ops/dev/sid…"),
+                    String::from("\""),
+                    String::from("2 weeks"),
+                    String::from("ago"),
+                    String::from("Up 44 seconds"),
+                    String::from(""),
+                    String::from("blah-sidekiq_2"),
+                ],
+            },
+            Row {
+                original_line: String::from("43484e7c2774   dd:latest                   \"ops/dev/proxy…\"   2 weeks ago   Up 46 seconds   0.0.0.0:80->80/tcp                 blah-proxy_4"),
+                cells: vec![
+                    String::from("43484e7c2774"),
+                    String::from("dd:latest"),
+                    String::from("\"ops/dev/proxy…"),
+                    String::from("\""),
+                    String::from("2 weeks"),
+                    String::from("ago"),
+                    String::from("Up 46 seconds"),
+                    String::from("0.0.0.0:80->80/tcp"),
+                    String::from("blah-proxy_4"),
+                ],
+            },
+
+            Row {
+                original_line: String::from("8a61b6cc2d3b   aaaaa:4.0.3-alpine          \"docker.s…\"        2 weeks ago   Up 46 seconds   0.0.0.0:6300->6322/tcp             blah.99_1"),
+                cells: vec![
+                    String::from("8a61b6cc2d3b"),
+                    String::from("aaaaa:4.0.3-alpine"),
+                    String::from("\"docker.s…\""),
+                    String::from(""),
+                    String::from("2 weeks"),
+                    String::from("ago"),
+                    String::from("Up 46 seconds"),
+                    String::from("0.0.0.0:6300->6322/tcp"),
+                    String::from("blah.99_1"),
+                ],
+            },
+
+
+    ])
+  }
+
+  #[test]
+  fn test_parse_git_status() {
+    let text = " M src/main.rs\n\
+                ?? src/parse/\n";
 
     assert_eq!(
       parse(String::from(text)),
-      vec![0, 15, 43, 60, 64, 72, 78, 94, 118, 129]
+      vec![
+        Row {
+          original_line: String::from(" M src/main.rs"),
+          cells: vec![String::from(" M"), String::from("src/main.rs"),],
+        },
+        Row {
+          original_line: String::from("?? src/parse/"),
+          cells: vec![String::from("??"), String::from("src/parse/"),],
+        },
+      ],
     )
   }
 
   #[test]
-  fn test_git_status() {
-    let text = " M src/main.rs\n\
-                ?? src/parse/\n";
-
-    assert_eq!(parse(String::from(text)), vec![0, 3])
-  }
-
-  #[test]
-  fn test_ls() {
+  fn test_parse_ls() {
     let text = "-rw-r--r--     1 user  staff     159 28 Apr  2020 Dockerfile\n\
                 -rw-r--r--     1 user  staff    7910 21 Sep 15:19 Readme.md\n\
                 drwxr-xr-x     3 user  staff      96 11 Apr  2020 docs";
 
     assert_eq!(
       parse(String::from(text)),
-      vec![0, 15, 17, 23, 32, 37, 40, 44, 50]
+      vec![
+        Row {
+          original_line: String::from(
+            "-rw-r--r--     1 user  staff     159 28 Apr  2020 Dockerfile"
+          ),
+          cells: vec![
+            String::from("-rw-r--r--"),
+            String::from("1"),
+            String::from("user"),
+            String::from("staff"),
+            String::from(" 159"),
+            String::from("28"),
+            String::from("Apr"),
+            String::from(" 2020"),
+            String::from("Dockerfile"),
+          ],
+        },
+        Row {
+          original_line: String::from(
+            "-rw-r--r--     1 user  staff    7910 21 Sep 15:19 Readme.md"
+          ),
+          cells: vec![
+            String::from("-rw-r--r--"),
+            String::from("1"),
+            String::from("user"),
+            String::from("staff"),
+            String::from("7910"),
+            String::from("21"),
+            String::from("Sep"),
+            String::from("15:19"),
+            String::from("Readme.md"),
+          ],
+        },
+        Row {
+          original_line: String::from("drwxr-xr-x     3 user  staff      96 11 Apr  2020 docs"),
+          cells: vec![
+            String::from("drwxr-xr-x"),
+            String::from("3"),
+            String::from("user"),
+            String::from("staff"),
+            String::from("  96"),
+            String::from("11"),
+            String::from("Apr"),
+            String::from(" 2020"),
+            String::from("docs"),
+          ],
+        },
+      ],
     )
   }
 }
