ae3bb545384e070cb245d8eb5aa7f196e98be2f7
Jesse Duffield <jessedduffield@gmail.com>
2021-01-05 17:02:54 +1100

    remove dead code

--- src/util/event.rs
+++ src/util/event.rs
@@ -5,14 +5,11 @@ use std::sync::{
   Arc,
 };
 use std::thread;
-use std::time::Duration;
-
 use termion::event::Key;
 use termion::input::TermRead;
 
 pub enum Event<I> {
   Input(I),
-  Tick,
 }
 
 /// A small event handler that wrap termion input and tick events. Each event
@@ -21,20 +18,17 @@ pub struct Events {
   rx: mpsc::Receiver<Event<Key>>,
   input_handle: thread::JoinHandle<()>,
   ignore_exit_key: Arc<AtomicBool>,
-  // tick_handle: thread::JoinHandle<()>,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Config {
   pub exit_key: Key,
-  // pub tick_rate: Duration,
 }
 
 impl Default for Config {
   fn default() -> Config {
     Config {
       exit_key: Key::Char('q'),
-      // tick_rate: Duration::from_millis(250),
     }
   }
 }
@@ -65,19 +59,10 @@ impl Events {
         }
       })
     };
-    // let tick_handle = {
-    //   thread::spawn(move || loop {
-    //     if tx.send(Event::Tick).is_err() {
-    //       break;
-    //     }
-    //     thread::sleep(config.tick_rate);
-    //   })
-    // };
     Events {
       rx,
       ignore_exit_key,
       input_handle,
-      // tick_handle,
     }
   }
 
