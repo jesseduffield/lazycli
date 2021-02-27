85c8b7d043c2f328453204507e47bf485ba173e4
Jesse Duffield <jessedduffield@gmail.com>
2021-02-27 11:47:38 +1100

    just use the directories crate for each platform

--- src/config/linux.rs
+++
@@ -1,9 +0,0 @@
-use std::{error::Error, path::PathBuf};
-
-use xdg::BaseDirectories;
-
-pub fn config_path() -> Result<PathBuf, Box<dyn Error>> {
-  let xdg_dirs = BaseDirectories::with_prefix("lazycli")?;
-
-  Ok(xdg_dirs.place_config_file("config.yml")?)
-}
--- src/config/storage.rs
+++ src/config/storage.rs
@@ -2,10 +2,33 @@ use std::{error::Error, fs::File, io::prelude::*, path::PathBuf};
 
 use super::Config;
 
-#[cfg_attr(target_os = "linux", path = "windows.rs")]
-#[cfg_attr(target_os = "macos", path = "windows.rs")]
-#[cfg_attr(windows, path = "windows.rs")]
-pub mod os;
+use std::fs;
+use std::io;
+use std::path::Path;
+
+extern crate directories;
+use directories::ProjectDirs;
+
+// adapted from xdg crate
+fn write_file<P>(home: &PathBuf, path: P) -> io::Result<PathBuf>
+where
+  P: AsRef<Path>,
+{
+  match path.as_ref().parent() {
+    Some(parent) => (fs::create_dir_all(home.join(parent)))?,
+    None => (fs::create_dir_all(home))?,
+  }
+  Ok(PathBuf::from(home.join(path.as_ref())))
+}
+
+pub fn config_path() -> Result<PathBuf, Box<dyn Error>> {
+  let config_dir = ProjectDirs::from("", "", "lazycli")
+    .unwrap()
+    .config_dir()
+    .to_owned();
+
+  Ok(write_file(&PathBuf::from(config_dir), "config.yml")?)
+}
 
 pub fn prepare_config(config_path: &PathBuf) -> Result<Config, Box<dyn Error>> {
   if config_path.exists() {
