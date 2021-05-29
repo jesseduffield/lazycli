ce54f897762b1c3e5d7274975c14acc578c0515e
Jesse Duffield <jessedduffield@gmail.com>
2021-05-29 15:31:54 +1000

    allow configuring env var for config directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 #[allow(dead_code)]
 use std::error::Error;
 
@@ -16,10 +17,11 @@ mod ui;
 use app::App;
 use args::Args;
 use config::storage;
+use storage::CONFIG_DIR_ENV_VAR;
 
 fn main() -> Result<(), Box<dyn Error>> {
   let args = Args::new();
-  let config_path = storage::config_path()?;
+  let config_path = storage::config_path(env::var(CONFIG_DIR_ENV_VAR).ok())?;
   let config = storage::prepare_config(&config_path)?;
 
   let app = App::new(&config, config_path, args);
