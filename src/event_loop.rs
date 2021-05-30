955b7c87963f78eacb8e65cc0bd835318ad42247
Jesse Duffield <jessedduffield@gmail.com>
2021-05-30 12:15:32 +1000

    potentially fix file opening on windows

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -206,11 +206,7 @@ fn handle_event(
               app,
               loading_tx,
               tx,
-              format!(
-                "{} \"{}\"",
-                os_commands::open_command(),
-                app.config_path.to_str().unwrap()
-              ),
+              os_commands::open_command(app.config_path.to_str().unwrap()),
             );
           }
           KeyCode::Char(c) => {
